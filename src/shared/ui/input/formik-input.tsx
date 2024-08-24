8b61ce10533f274e070dc05161f189481646aebe
Rocky <atoyanrobert21@gmail.com>
2024-08-24 08:03:47 +0300

    add icon to formik-input.tsx

--- src/shared/ui/input/formik-input.tsx
+++ src/shared/ui/input/formik-input.tsx
@@ -5,5 +5,17 @@ import { Input } from "@/shared/ui";
 
 export const FormikInput = (props) => {
   const [field, meta, helpers] = useField(props);
+  if (props.Icon) {
+    return (
+      <div className={"relative text-black/40 flex items-stretch gap-2"}>
+        <props.Icon
+          className={
+            "absolute w-5 top-1/2 -translate-y-1/2 -translate-x-1/2 left-6 "
+          }
+        />
+        <Input className={"pl-12 text-black"} {...field} {...props} />
+      </div>
+    );
+  }
   return <Input {...field} {...props} />;
 };
